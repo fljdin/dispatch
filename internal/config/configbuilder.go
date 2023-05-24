fljdin/dispatch

commit 8935956ea3063ce9ecf983f385445efb94c0233d

feat: add log flag

internal/config/configbuilder.go
@@ -32,6 +32,11 @@ func (cb *ConfigBuilder) WithDefaultConnection(cnx models.Connection) *ConfigBui
 	return cb
 }
 
+func (cb *ConfigBuilder) WithLogfile(filename string) *ConfigBuilder {
+	cb.config.Logfile = filename
+	return cb
+}
+
 func (cb *ConfigBuilder) WithYAML(yamlString string) *ConfigBuilder {
 	if cb.err != nil {
 		return cb

internal/cmd/root.go
@@ -14,16 +14,18 @@ import (
 var (
 	argConfigFilename     string
 	argConfigFilenameDesc string = "configuration file"
+	argLogfile            string
+	argLogfileDesc        string = "log file"
 	argMaxWorkers         int
 	argMaxWorkersDesc     string = "number of workers (default 2)"
 	argPgDbname           string
 	argPgDbnameDesc       string = "database name to connect to"
 	argPgHost             string
 	argPgHostDesc         string = "database server host or socket directory"
-	argPgPwdPrompt        bool
-	argPgPwdPromptdDesc   string = "force password prompt"
 	argPgPort             int
 	argPgPortDesc         string = "database server port"
+	argPgPwdPrompt        bool
+	argPgPwdPromptdDesc   string = "force password prompt"
 	argPgUser             string
 	argPgUserDesc         string = "database user name"
 	argSqlFilename        string
@@ -81,6 +83,8 @@ func Execute() {
 func init() {
 	rootCmd.PersistentFlags().StringVarP(&argConfigFilename, "config", "c", "", argConfigFilenameDesc)
 	rootCmd.PersistentFlags().BoolVarP(&argVerbose, "verbose", "v", false, argVerboseDesc)
+	rootCmd.PersistentFlags().StringVarP(&argLogfile, "log", "l", "", argLogfileDesc)
+
 	rootCmd.PersistentFlags().StringVarP(&argPgHost, "host", "h", "", argPgHostDesc)
 	rootCmd.PersistentFlags().IntVarP(&argPgPort, "port", "p", 0, argPgPortDesc)
 	rootCmd.PersistentFlags().StringVarP(&argPgDbname, "dbname", "d", "", argPgDbnameDesc)

internal/cmd/run.go
@@ -31,6 +31,8 @@ func newConfig() (config.Config, error) {
 			WithMaxWorkers(argMaxWorkers)
 	}
 
+	configBuild.WithLogfile(argLogfile)
+
 	argPgPassword := ReadHiddenInput("Password: ", argPgPwdPrompt)
 	defaultConnection = models.Connection{
 		Host:     argPgHost,
@@ -48,7 +50,7 @@ func newDispatcher(config config.Config) (dispatcher.Dispatcher, error) {
 	return dispatcher.NewDispatcherBuilder(context.Background()).
 		WithWorkerNumber(config.MaxWorkers).
 		WithMemorySize(len(config.Tasks)).
-		WithTraceFile(config.Summary).
+		WithLogfile(config.Logfile).
 		WithConsole().
 		Build()
 }

internal/config/config.go
@@ -13,7 +13,7 @@ var ConfigWorkersDefault int = 2
 type Config struct {
 	Tasks             []models.Task      `yaml:"tasks"`
 	MaxWorkers        int                `yaml:"workers"`
-	Summary           string             `yaml:"summary"`
+	Logfile           string             `yaml:"logfile"`
 	Connections       models.Connections `yaml:"connections"`
 	DefaultConnection models.Connection
 }

internal/dispatcher/dispatcherbuilder.go
@@ -9,7 +9,7 @@ import (
 
 type DispatcherBuilder struct {
 	dispatcher     Dispatcher
-	traceFilename  string
+	logfileName    string
 	memorySize     int
 	consoleEnabled bool
 	err            error
@@ -29,8 +29,8 @@ func NewDispatcherBuilder(ctx context.Context) *DispatcherBuilder {
 	}
 }
 
-func (db *DispatcherBuilder) WithTraceFile(filename string) *DispatcherBuilder {
-	db.traceFilename = filename
+func (db *DispatcherBuilder) WithLogfile(filename string) *DispatcherBuilder {
+	db.logfileName = filename
 	return db
 }
 
@@ -68,7 +68,7 @@ func (db *DispatcherBuilder) Build() (Dispatcher, error) {
 		context: db.dispatcher.context,
 	}
 
-	if err := db.dispatcher.observer.WithTrace(db.traceFilename); err != nil {
+	if err := db.dispatcher.observer.WithTrace(db.logfileName); err != nil {
 		db.err = err
 	}
 

internal/dispatcher/dispatcherbuilder_test.go
@@ -20,7 +20,7 @@ func TestDispatcherBuilderWithInvalidMemorySize(t *testing.T) {
 
 func TestDispatcherBuilderWithInvalidTraceFile(t *testing.T) {
 	_, err := NewDispatcherBuilder(context.Background()).
-		WithTraceFile("not/exists.out").
+		WithLogfile("not/exists.out").
 		Build()
 
 	require.NotNil(t, err)

commit 33713406ab790e6bf00d4a6e81481ebe256dc9da

fix: does not override file defined in YAML
bug introduced in 8935956ea3063ce9ecf983f385445efb94c0233d

internal/config/configbuilder.go
@@ -37,6 +37,10 @@ func (cb *ConfigBuilder) WithDefaultConnection(cnx models.Connection) *ConfigBui
 }
 
 func (cb *ConfigBuilder) WithLogfile(filename string) *ConfigBuilder {
+	if len(filename) == 0 {
+		return cb
+	}
+
 	cb.config.Logfile = filename
 	return cb
 }