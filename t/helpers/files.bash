c58738cb863df5413b4dfc6ec15f9351cf87987a
Florent Jardin <florent.jardin@gmail.com>
2023-06-27 15:44:09 +0200

    fix: reviewed by bersace

--- t/helpers/files.bash
+++ t/helpers/files.bash
@@ -6,12 +6,12 @@ function create_commands() {
     cat <<-EOF > commands.sh
 	echo 1
 	echo 2
-EOF
+	EOF
 }
 
 function create_queries() {
     cat <<-EOF > queries.sql
 	SELECT 1;
 	SELECT 2;
-EOF
+	EOF
 }
